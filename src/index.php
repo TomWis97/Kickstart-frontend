fdbf3f20a57d54818839a0f6eb4b1ab022f253d2
TomWis97 <github@tomwishaupt.nl>
2017-02-24 15:19:18 +0100

    Placeholder files

---
+++ src/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Kickstart-frontend</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <head>
+    <body>
+        <div id="container">
+            <div id="header">
+                <h1>Kickstart-frontend</h1>
+            </div>
+            <div id="contents">
+                <a href="edit.php?id=1">
+                    <div class="newhost">
+                        <h2>Hostname</h2>
+                        <ul>
+                            <li>10.11.12.13</li>
+                        </ul>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </body>
+</html>
